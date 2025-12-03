7fde5f7670d7880e24460ea00ce74b7e4086a743
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-12-03 20:04:28 +0200

    feat: implement AppLayout and integrate CartOverview component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Fast Pizza Resturant</Link>
+
+      <p>User</p>
+    </header>
+  );
+}
