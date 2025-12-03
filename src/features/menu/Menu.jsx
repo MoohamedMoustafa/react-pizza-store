9e83116bcdbba0e74e0c11228adc451f2428eb44
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-12-03 21:28:06 +0200

    feat: implement menu display and integrate MenuItem component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
 
-  return <h1>Menu</h1>;
+  return (
+    <ul>
+      {menu.length > 0 &&
+        menu.map((item) => <MenuItem key={item.id} pizza={item} />)}
+    </ul>
+  );
 }
 
 export async function loader() {
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 
 export default function AppLayout() {
+  const naviation = useNavigation();
+  const isLoading = naviation.state === "loading";
+  console.log(isLoading);
+
   return (
     <div>
       <Header />
