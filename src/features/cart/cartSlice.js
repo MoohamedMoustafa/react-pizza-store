0ba762df32fd87464cc8562f23534b9a941c3787
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-12-08 14:50:49 +0200

    feat: add cart slice for managing cart state and integrate with store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: 'Mediterranean',
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export const {
+  addItem,
+  clearCart,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = cartSlice.actions;
